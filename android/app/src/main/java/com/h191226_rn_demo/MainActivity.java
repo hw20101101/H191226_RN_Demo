8f0ccbfb92fe28ad1ac9d7eff5e19ed54e216c38
hw20101101 <1296324895@qq.com>
2020-01-17 23:18:52 +0800

    about android code

---
+++ android/app/src/main/java/com/h191226_rn_demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.h191226_rn_demo;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "H191226_RN_Demo";
+  }
+}
